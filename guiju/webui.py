396cdef4664c0af3ea0b8df184cabb02669a56e3
ray <uubs@yeah.net>
2023-06-27 19:05:13 +0800

    update add debug mode

--- guiju/webui.py
+++ guiju/webui.py
@@ -22,14 +22,14 @@ from modules.shared import cmd_opts
 def get_prompt(_gender, _age, _viewpoint):
     age_prompts = ['child', 'youth', 'middlescent']
     if _gender == 0:
-        sd_positive_prompt = f"(RAW photo, best quality), (realistic, photo-realistic:1.3), masterpiece, an extremely delicate and beautiful, extremely detailed, CG, unity , 2k wallpaper, Amazing, finely detail, extremely detailed CG unity 8k wallpaper, ultra-detailed, highres, beautiful detailed girl, {age_prompts[_age]}, detailed fingers, 1girl, young, realistic body, fluffy black hair, girl posing for a photo, good hand, (simple background:1.3), (white background:1.3),(full body:1.5)"
+        sd_positive_prompt = f"(RAW photo, best quality), (realistic, photo-realistic:1.3), masterpiece, an extremely delicate and beautiful, extremely detailed, CG, unity , 2k wallpaper, Amazing, finely detail, extremely detailed CG unity 8k wallpaper, ultra-detailed, highres, beautiful detailed girl, {age_prompts[_age]}, detailed fingers, 1girl, young, realistic body, fluffy black hair, girl posing for a photo, good hand, (simple background:1.3), (white background:1.3),(full body:1.5), light smile, beautiful detailed nose, beautiful detailed eyes, long eyelashes"
         if _age != 2:
             sd_positive_prompt += ',<lora:shojovibe_v11:0.4> ,<lora:koreanDollLikeness:0.4>'
     else:
-        sd_positive_prompt = f'(RAW photo, best quality), (realistic, photo-realistic:1.3), masterpiece, an extremely delicate, extremely detailed, CG, unity , 2k wallpaper, Amazing, finely detail, extremely detailed CG unity 8k wallpaper, ultra-detailed, highres,1boy, realistic body, (simple background:1.3), (white background:1.3), {age_prompts[_age]}, (full body:1.3)'
+        sd_positive_prompt = f'(RAW photo, best quality), (realistic, photo-realistic:1.3), masterpiece, an extremely delicate, extremely detailed, CG, unity , 2k wallpaper, Amazing, finely detail, extremely detailed CG unity 8k wallpaper, ultra-detailed, highres, (1boy:1.3), realistic body, (simple background:1.3), (white background:1.3), {age_prompts[_age]}, (full body:1.3), detailed nose, detailed eyes'
 
     if _viewpoint == 0:
-        sd_positive_prompt += ',light smile, beautiful detailed nose, beautiful detailed eyes, long eyelashes, realistic face, extremely detailed eyes and face, light on face, looking at viewer'
+        sd_positive_prompt += ', realistic face, extremely detailed eyes and face, light on face, looking at viewer'
     elif _viewpoint == 1:
         sd_positive_prompt += ',(side face:1.5), side view of face, lateral face, looking to the side'
     else:
