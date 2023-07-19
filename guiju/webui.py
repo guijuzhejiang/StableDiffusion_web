77fb990bcdbcbec1dc92dc6590379c6bce1577de
ray <uubs@yeah.net>
2023-07-19 09:47:48 +0800

    update adetail

--- guiju/webui.py
+++ guiju/webui.py
@@ -281,7 +281,7 @@ def proceed_cloth_inpaint(_batch_size, _input_image, _gender, _age, _viewpoint_m
     face_args = {'ad_model': 'face_yolov8m.pt', 'ad_prompt': '', 'ad_negative_prompt': '', 'ad_confidence': 0.3,
                  'ad_mask_min_ratio': 0, 'ad_mask_max_ratio': 1, 'ad_x_offset': 0, 'ad_y_offset': 0,
                  'ad_dilate_erode': 4, 'ad_mask_merge_invert': 'None', 'ad_mask_blur': 4, 'ad_denoising_strength': 0.4,
-                 'ad_inpaint_only_masked': False, 'ad_inpaint_only_masked_padding': 32,
+                 'ad_inpaint_only_masked': True, 'ad_inpaint_only_masked_padding': 32,
                  'ad_use_inpaint_width_height': False, 'ad_inpaint_width': 512, 'ad_inpaint_height': 512,
                  'ad_use_steps': False, 'ad_steps': 28, 'ad_use_cfg_scale': False, 'ad_cfg_scale': 7,
                  'ad_use_noise_multiplier': False, 'ad_noise_multiplier': 1, 'ad_restore_face': False,
