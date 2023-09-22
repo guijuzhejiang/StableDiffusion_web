2185adf71cde8131446db7866d999ef7482c51b7
zzg <uubs@yeah.net>
2023-09-22 09:26:08 +0800

    add hand lora

--- lora_config.py
+++ lora_config.py
@@ -2,7 +2,7 @@ import shutil
 
 lora_model_common_dict = [
     {'lora_name': 'polyhedron_new_skin_v1.1', 'weight': 0.1, 'label': '赋予真实皮肤，带褶皱'},
-    {'lora_name': 'add_detail', 'weight': 1, 'label': '增加细节'},
+    {'lora_name': 'add_detail', 'weight': 2, 'label': '增加细节'},
     {'lora_name': 'more_details', 'weight': 1.5, 'label': '增加细节'},
     # {'lora_name': 'k Hand Mix 101_v1.0', 'weight': 0.6, 'label': '手部修复'},
     # {'lora_name': 'Xian-T手部修复lora（不用controlnet也不坏手了）_v3.0', 'weight': 1, 'label': '手部修复'},
