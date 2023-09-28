ccbef782d23fae205e6401e24d5a2b6e9ad34c9d
zzg <uubs@yeah.net>
2023-09-28 09:05:11 +0800

    add hand lora

--- guiju/predictor_opennsfw2.py
+++ guiju/predictor_opennsfw2.py
@@ -1,38 +1,38 @@
 import threading
 import opennsfw2
-# import tensorflow as tf
-# config = tf.compat.v1.ConfigProto()
-# config.gpu_options.allow_growth = True
-# tf.compat.v1.keras.backend.set_session(tf.compat.v1.Session(config=config))
-# from keras import Model
-#
-#
-# PREDICTOR = None
-# THREAD_LOCK = threading.Lock()
-# MAX_PROBABILITY = 0.97
-#
-#
-# def get_predictor() -> Model:
-#     global PREDICTOR
-#
-#     with THREAD_LOCK:
-#         if PREDICTOR is None:
-#             PREDICTOR = opennsfw2.make_open_nsfw_model()
-#     return PREDICTOR
-#
-#
-# def clear_predictor() -> None:
-#     global PREDICTOR
-#     PREDICTOR = None
+import tensorflow as tf
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.allow_growth = True
+tf.compat.v1.keras.backend.set_session(tf.compat.v1.Session(config=config))
+from keras import Model
+
+
+PREDICTOR = None
+THREAD_LOCK = threading.Lock()
+MAX_PROBABILITY = 0.96
+
+
+def get_predictor() -> Model:
+    global PREDICTOR
+
+    with THREAD_LOCK:
+        if PREDICTOR is None:
+            PREDICTOR = opennsfw2.make_open_nsfw_model()
+    return PREDICTOR
+
+
+def clear_predictor() -> None:
+    global PREDICTOR
+    PREDICTOR = None
 #
 #
 def predict_image(target_path: str) -> bool:
-    # nsfw_probability = opennsfw2.predict_image(target_path)
-    # print(f'nsfw probability:{nsfw_probability}')
-    # return nsfw_probability > MAX_PROBABILITY
-    return False
+    nsfw_probability = opennsfw2.predict_image(target_path)
+    print(f'nsfw probability:{nsfw_probability}')
+    return nsfw_probability > MAX_PROBABILITY
+    # return False
 #
 #
-# def predict_video(target_path: str) -> bool:
-#     _, probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100)
-#     return any(probability > MAX_PROBABILITY for probability in probabilities)
+def predict_video(target_path: str) -> bool:
+    _, probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100)
+    return any(probability > MAX_PROBABILITY for probability in probabilities)
