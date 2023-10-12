ba5c7c558277c72f89acee138c4dade6fe34685e
ray <uubs@yeah.net>
2023-10-12 09:46:49 +0800

    update preprocess

--- handlers/websocket.py
+++ handlers/websocket.py
@@ -21,139 +21,136 @@ async def sd_genreate(request: Request, ws):
         account = \
         (await request.app.ctx.supabase_client.atable("account").select("*").eq("id", user_id).execute()).data[0]
 
-        while True:
-            try:
-                flag_send_task = False
-                # recv params
-                raw_msg = await ws.recv()
+        try:
+            flag_send_task = False
+            # recv params
+            raw_msg = await ws.recv()
 
-                package = ujson.loads(raw_msg)
-                params = package['params']
-                format_package = {'mode': [package['mode']], 'user_id': [package['user_id']], 'params': [ujson.dumps(params)], 'input_image': [['']]}
+            package = ujson.loads(raw_msg)
+            params = package['params']
+            format_package = {'mode': [package['mode']], 'user_id': [package['user_id']], 'params': [ujson.dumps(params)], 'input_image': [['']]}
 
-                # cal prices
-                cost_points = 10
-                if package['mode'] == 'hires':
-                    _output_width = int(params['output_width'])
-                    _output_height = int(params['output_height'])
+            # cal prices
+            cost_points = 10
+            if package['mode'] == 'hires':
+                _output_width = int(params['output_width'])
+                _output_height = int(params['output_height'])
 
-                    cost_points = 1
-                    pixel_sum = _output_width + _output_height
-                    if pixel_sum >= 2561:
-                        cost_points = 16
-                    if pixel_sum >= 4681:
-                        cost_points = 20
+                cost_points = 1
+                pixel_sum = _output_width + _output_height
+                if pixel_sum >= 2561:
+                    cost_points = 16
+                if pixel_sum >= 4681:
+                    cost_points = 20
+            else:
+                cost_points *= int(int(params['batch_size']))
+
+            # check balance
+            account = (await request.app.ctx.supabase_client.atable("account").select("*").eq("id", user_id).execute()).data[0]
+            buf_result = {'success': True, 'result': None, 'act': None, 'type': package['mode']}
+            if cost_points <= account['balance']:
+                start = datetime.datetime.now()
+
+                # recv image
+                dir_path = os.path.join(CONFIG['storage_dirpath']['user_upload'])
+                async with aiofile.async_open(os.path.join(dir_path, f"{user_id}.png"), 'rb') as file:
+                    image_data = await file.read()
+                if package['mode'] == 'model':
+                    format_package['input_image'][0].append(image_data)
                 else:
-                    cost_points *= int(int(params['batch_size']))
-
-                # check balance
-                account = \
-                (await request.app.ctx.supabase_client.atable("account").select("*").eq("id", user_id).execute()).data[
-                    0]
-                buf_result = {'success': True, 'result': None, 'act': None, 'type': package['mode']}
-                if cost_points <= account['balance']:
-                    start = datetime.datetime.now()
-
-                    # recv image
-                    dir_path = os.path.join(CONFIG['storage_dirpath']['user_upload'])
-                    async with aiofile.async_open(os.path.join(dir_path, f"{user_id}.png"), 'rb') as file:
-                        image_data = await file.read()
-                    if package['mode'] == 'model':
+                    parsed_url = urlparse(package['chosen_image'])
+                    # 获取查询参数
+                    query_params = parse_qs(parsed_url.query)
+                    img_fp = os.path.join(CONFIG['storage_dirpath']['user_dir'], query_params['uid'][0], query_params['imgpath'][0])
+                    with open(img_fp, "rb") as image_file:
+                        # 读取二进制数据
+                        image_data = image_file.read()
                         format_package['input_image'][0].append(image_data)
-                    else:
-                        parsed_url = urlparse(package['chosen_image'])
-                        # 获取查询参数
-                        query_params = parse_qs(parsed_url.query)
-                        img_fp = os.path.join(CONFIG['storage_dirpath']['user_dir'], query_params['uid'][0], query_params['imgpath'][0])
-                        with open(img_fp, "rb") as image_file:
-                            # 读取二进制数据
-                            image_data = image_file.read()
-                            format_package['input_image'][0].append(image_data)
 
-                    # send task
-                    task_result = request.app.ctx.sd_workshop(**format_package)
-                    await ws.send(ujson.dumps({'success': True, 'result': str(task_result), 'act': 'save_task_id', 'type': package['mode']}))
-                    flag_send_task = True
-                    await request.app.ctx.redis_session.rpush('celery_task_queue', str(task_result))
+                # send task
+                task_result = request.app.ctx.sd_workshop(**format_package)
+                await ws.send(ujson.dumps({'success': True, 'result': str(task_result), 'act': 'save_task_id', 'type': package['mode']}))
+                flag_send_task = True
+                await request.app.ctx.redis_session.rpush('celery_task_queue', str(task_result))
 
-                    print('wait')
-                    while not task_result.ready():
-                        print(task_result.state)
-                        if task_result.state == 'PROGRESS':
-                            buf_result['act'] = 'show_progress'
-                            if task_result.info is not None:
-                                try:
-                                    buf_result['result'] = task_result.info['progress']
-                                    await ws.send(ujson.dumps(buf_result))
-                                except Exception:
-                                    buf_result['result'] = 99
-                                    await ws.send(ujson.dumps(buf_result))
-
-                        elif task_result.state == 'PENDING':
-                            buf_result['act'] = 'show_queue'
+                print('wait')
+                while not task_result.ready():
+                    print(task_result.state)
+                    if task_result.state == 'PROGRESS':
+                        buf_result['act'] = 'show_progress'
+                        if task_result.info is not None:
                             try:
-                                queue_list = await request.app.ctx.redis_session.lrange('celery_task_queue', 0, -1)
-                                if queue_list:
-                                    if str(task_result) in queue_list:
-                                        buf_result['result'] = f"第{queue_list.index(str(task_result)) + 1}位"
-                                    else:
-                                        buf_result['result'] = f"..."
+                                buf_result['result'] = task_result.info['progress']
+                                await ws.send(ujson.dumps(buf_result))
+                            except Exception:
+                                buf_result['result'] = 99
+                                await ws.send(ujson.dumps(buf_result))
+
+                    elif task_result.state == 'PENDING':
+                        buf_result['act'] = 'show_queue'
+                        try:
+                            queue_list = await request.app.ctx.redis_session.lrange('celery_task_queue', 0, -1)
+                            if queue_list:
+                                if str(task_result) in queue_list:
+                                    buf_result['result'] = f"第{queue_list.index(str(task_result)) + 1}位"
                                 else:
                                     buf_result['result'] = f"..."
-                                await ws.send(ujson.dumps(buf_result))
-
-                                # queue_list = task_result.app.control.inspect().reserved()[f'celery@{sd_workshop.op.__name__}_worker']
-
-                                # get_success = False
-                                # for index, q in enumerate(queue_list):
-                                #     if str(task_result) == q['id']:
-                                #         get_success = True
-                                #         buf_result['result'] = f"第{index+1}位"
-                                #         await ws.send(ujson.dumps(buf_result))
-                                # else:
-                                #     if not get_success:
-                                #         buf_result['result'] = f"..."
-                                #         await ws.send(ujson.dumps(buf_result))
-
-                            except Exception:
-                                print("fuck1")
-                                logging(
-                                    f"[websocket fatal error][{datetime.datetime.now()}]:"
-                                    f"{traceback.format_exc()}",
-                                    f"logs/error.log", print_msg=True)
+                            else:
                                 buf_result['result'] = f"..."
-                                await ws.send(ujson.dumps(buf_result))
-
-                        elif task_result.state == 'SUCCESS':
-                            buf_result['act'] = 'show_progress'
-                            buf_result['result'] = 100
                             await ws.send(ujson.dumps(buf_result))
-                            break
 
-                        elif task_result.state == 'REVOKED':
-                            break
+                            # queue_list = task_result.app.control.inspect().reserved()[f'celery@{sd_workshop.op.__name__}_worker']
 
-                        await asyncio.sleep(0.5)
-                    print('done.')
-                    task_result = task_result.result
-                    task_result['act'] = f"show_result"
-                    task_result['type'] = package['mode']
-                    if task_result['success']:
-                        data = await request.app.ctx.supabase_client.atable("transaction").insert({"user_id": user_id,
-                                                                                                   'amount': cost_points,
-                                                                                                   'is_plus': False,
-                                                                                                   'status': 1,
-                                                                                                   }).execute()
-                        res = (await request.app.ctx.supabase_client.atable("account").update(
-                            {"balance": account['balance'] - cost_points}).eq("id", user_id).execute()).data
+                            # get_success = False
+                            # for index, q in enumerate(queue_list):
+                            #     if str(task_result) == q['id']:
+                            #         get_success = True
+                            #         buf_result['result'] = f"第{index+1}位"
+                            #         await ws.send(ujson.dumps(buf_result))
+                            # else:
+                            #     if not get_success:
+                            #         buf_result['result'] = f"..."
+                            #         await ws.send(ujson.dumps(buf_result))
 
-                else:
-                    task_result = {'success': False, 'result': "余额不足"}
+                        except Exception:
+                            print("fuck1")
+                            logging(
+                                f"[websocket fatal error][{datetime.datetime.now()}]:"
+                                f"{traceback.format_exc()}",
+                                f"logs/error.log", print_msg=True)
+                            buf_result['result'] = f"..."
+                            await ws.send(ujson.dumps(buf_result))
 
-                await ws.send(ujson.dumps(task_result))
-                break
-            except asyncio.TimeoutError:
-                print("fuck2")
+                    elif task_result.state == 'SUCCESS':
+                        buf_result['act'] = 'show_progress'
+                        buf_result['result'] = 100
+                        await ws.send(ujson.dumps(buf_result))
+                        break
+
+                    elif task_result.state == 'REVOKED':
+                        break
+
+                    await asyncio.sleep(0.5)
+                print('done.')
+                task_result = task_result.result
+                task_result['act'] = f"show_result"
+                task_result['type'] = package['mode']
+                if task_result['success']:
+                    data = await request.app.ctx.supabase_client.atable("transaction").insert({"user_id": user_id,
+                                                                                               'amount': cost_points,
+                                                                                               'is_plus': False,
+                                                                                               'status': 1,
+                                                                                               }).execute()
+                    res = (await request.app.ctx.supabase_client.atable("account").update(
+                        {"balance": account['balance'] - cost_points}).eq("id", user_id).execute()).data
+
+            else:
+                task_result = {'success': False, 'result': "余额不足"}
+
+            await ws.send(ujson.dumps(task_result))
+
+        except asyncio.TimeoutError:
+            print("fuck2")
 
     except Exception:
         print("fuck3")
