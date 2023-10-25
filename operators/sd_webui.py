2b8a0c1b12aad6e798b398fb47f70bfac72aec38
ray <uubs@yeah.net>
2023-10-25 10:22:18 +0800

    update hair generate

--- operators/sd_webui.py
+++ operators/sd_webui.py
@@ -1322,7 +1322,7 @@ class OperatorSD(Operator):
                     _input_image.save(origin_image_path, format='PNG')
 
                     # preprocess
-                    sam_result, person_boxes = self.sam_h.sam_predict(self.dino_model_name, 'head.hair',
+                    sam_result, person_boxes = self.sam_h.sam_predict(self.dino_model_name, 'face.hair',
                                                                       0.4,
                                                                       _input_image.convert('RGBA'))
                     if len(sam_result) == 0:
@@ -1341,16 +1341,14 @@ class OperatorSD(Operator):
                         if person_box[1] < 0:
                             person_box[1] = 0
                         person_box[2] = person_box[2] + int(person_width*0.2)
-                        if person_box[2] > _input_image_width:
-                            person_box[2] = _input_image_width
+                        if person_box[2] >= _input_image_width:
+                            person_box[2] = _input_image_width-1
                         person_box[3] = person_box[3] + int(person_height*0.8)
-                        if person_box[3] > _input_image_height:
-                            person_box[3] = _input_image_height
+                        if person_box[3] >= _input_image_height:
+                            person_box[3] = _input_image_height-1
 
                         person_width = person_box[2] - person_box[0]
                         person_height = person_box[3] - person_box[1]
-                        if person_box[3] > _input_image_height:
-                            person_box[3] = _input_image_height
 
                         if person_width < person_height:
                             padding_left = int((person_height-person_width) / 2)
