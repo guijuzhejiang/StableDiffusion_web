4a5ec4d817b977703f4630e3f6e6d7cfd8d78305
zzg <uubs@yeah.net>
2023-10-25 16:06:40 +0800

    add hand lora

--- operators/sd_webui.py
+++ operators/sd_webui.py
@@ -1346,7 +1346,7 @@ class OperatorSD(Operator):
                         person_box[2] = person_box[2] + int(person_width*0.15)
                         if person_box[2] >= _input_image_width:
                             person_box[2] = _input_image_width-1
-                        person_box[3] = person_box[3] + int(person_height*0.6)
+                        person_box[3] = person_box[3] + int(person_height*0.5)
                         if person_box[3] >= _input_image_height:
                             person_box[3] = _input_image_height-1
 
