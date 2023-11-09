8be01099d920ee80277ea8f90b4261e99fb42d59
zzg <uubs@yeah.net>
2023-11-09 11:03:53 +0800

    add hand lora

--- operators/sd_webui.py
+++ operators/sd_webui.py
@@ -1987,7 +1987,7 @@ class OperatorSD(Operator):
                             # bottom_ratio = min(0.58, math.pow(person0_width / person0_height,
                             #                                   factor_bottom) * constant_bottom)
                             top_ratio = 0.5
-                            bottom_ratio = 0.7
+                            bottom_ratio = 0.65
                             print(f"bottom_ratio1: {math.pow(person0_width / person0_height, factor_bottom) * constant_bottom}")
                             print(f"bottom_ratio: {bottom_ratio}")
                             print(f"top_ratio1: {math.pow(person0_width / person0_height, factor_top) * constant_top}")
