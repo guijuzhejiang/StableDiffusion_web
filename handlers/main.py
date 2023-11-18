9413475c62969f036d81bf4f220d4458df26d237
ray <uubs@yeah.net>
2023-11-18 09:44:02 +0800

    update local debug

--- handlers/main.py
+++ handlers/main.py
@@ -156,7 +156,7 @@ class QueryDiscount(HTTPMethodView):
             start_date = date(2023, 11, 1)
             end_date = date(2023, 11, 30)
             if start_date <= date.today() <= end_date:
-                res.append(['十一月优惠礼', 0.6])
+                res.append(['十一月优惠礼6折', 0.6])
 
             if len(res) > 1:
                 res.append(['多个折扣可叠加', 1])
