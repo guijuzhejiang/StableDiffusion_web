6b1872f2edcf86c356a4498b6f1cdfa8fdecf8cc
ray <uubs@yeah.net>
2023-11-19 19:28:15 +0800

    update avatar

--- handlers/websocket.py
+++ handlers/websocket.py
@@ -94,6 +94,8 @@ async def sd_genreate(request: Request, ws):
                         dir_storage_path = CONFIG['storage_dirpath']['user_hair_dir']
                     elif category == 'mirror':
                         dir_storage_path = CONFIG['storage_dirpath']['user_mirror_dir']
+                    elif category == 'avatar':
+                        dir_storage_path = CONFIG['storage_dirpath']['user_avatar_dir']
                     else:
                         dir_storage_path = CONFIG['storage_dirpath']['user_dir']
                     img_fp = os.path.join(dir_storage_path, query_params['uid'][0], query_params['imgpath'][0])
