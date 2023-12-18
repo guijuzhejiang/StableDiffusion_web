22524cc5030af5c896106eeec41326f24a0f0c42
ray <uubs@yeah.net>
2023-12-18 15:34:19 +0800

    update hair

--- operators/sd_webui.py
+++ operators/sd_webui.py
@@ -2783,10 +2783,10 @@ class OperatorSD(Operator):
 
                         # crop
                         if _haircut_enable:
-                            new_person_box[0] = person_box[0] - int(person_width * 0.8)
-                            new_person_box[1] = person_box[1] - int(person_height * 0.8)
-                            new_person_box[2] = person_box[2] + int(person_width * 0.8)
-                            new_person_box[3] = person_box[3] + int(person_height * 0.8)
+                            new_person_box[0] = person_box[0] - int(person_width * 0.6)
+                            new_person_box[1] = person_box[1] - int(person_height * 0.33)
+                            new_person_box[2] = person_box[2] + int(person_width * 0.6)
+                            new_person_box[3] = person_box[3] + int(person_height * 0.6)
                             if new_person_box[0] < 0:
                                 new_person_box[0] = 0
                             if new_person_box[1] < 0:
