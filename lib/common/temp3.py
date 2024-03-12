4bdd44896d26dfe211ef5d030330b4676e41edec
zzg <uubs@yeah.net>
2024-03-12 09:48:53 +0800

    update sam args

--- lib/common/temp3.py
+++ lib/common/temp3.py
@@ -1,32 +1,4 @@
-import glob
-import sys
-from datetime import datetime, timedelta
 from collections import OrderedDict
-from PIL import Image
-import os
-
-
-from PIL import Image
-import os
-
-def convert_png_to_webp(folder_path):
-    # 遍历指定文件夹内的所有文件
-    for filename in os.listdir(folder_path):
-        if filename.endswith(".png"):  # 检查文件扩展名是否为 .png
-            # 构造完整的文件路径
-            file_path = os.path.join(folder_path, filename)
-            # 打开 PNG 图片
-            image = Image.open(file_path)
-            # 构造 WebP 的文件名
-            webp_filename = filename[:-4] + '.webp'
-            webp_path = os.path.join(folder_path, webp_filename)
-            # 保存为 WebP 格式
-            image.save(webp_path, 'WEBP')
-
-# 调用函数，替换下面的 'your_folder_path' 为你的 PNG 文件所在的文件夹路径
-# convert_png_to_webp('your_folder_path')
-
-
 import pandas as pd
 
 # 指定 Excel 文件路径
